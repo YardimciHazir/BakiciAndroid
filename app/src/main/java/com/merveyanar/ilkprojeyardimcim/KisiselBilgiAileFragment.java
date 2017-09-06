906ad9709aaba69e2a4109737a75122e6e6717a4
YardimciHazir <30574709+YardimciHazir@users.noreply.github.com>
2017-09-06 21:48:14 +0300

    Aile geri tusu eklendi

--- app/src/main/java/com/merveyanar/ilkprojeyardimcim/KisiselBilgiAileFragment.java
+++ app/src/main/java/com/merveyanar/ilkprojeyardimcim/KisiselBilgiAileFragment.java
@@ -1,12 +1,16 @@
 package com.merveyanar.ilkprojeyardimcim;
 
+import android.app.FragmentManager;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,30 +24,19 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link KisiselBilgiAileFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link KisiselBilgiAileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class KisiselBilgiAileFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    //private EditText aileFirstName,aileLastName,aileAddress;
-
     ArrayList<AileModel> aileList;
     ListView listView;
+    Button btnGeri;
 
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
@@ -51,21 +44,10 @@ public class KisiselBilgiAileFragment extends Fragment {
     private DatabaseReference databaseReferenceAile;
     private FirebaseUser firebaseUser;
 
-    //private DatabaseReference databaseReference;
 
     public KisiselBilgiAileFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment KisiselBilgiAileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static KisiselBilgiAileFragment newInstance(String param1, String param2) {
         KisiselBilgiAileFragment fragment = new KisiselBilgiAileFragment();
         Bundle args = new Bundle();
@@ -82,23 +64,30 @@ public class KisiselBilgiAileFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_kisisel_bilgi_aile, container, false);
 
         View view = inflater.inflate(R.layout.fragment_kisisel_bilgi_aile, container, false);
 
-        aileList=new ArrayList<AileModel>();
-        listView= (ListView) view.findViewById(R.id.listView);
-        database= FirebaseDatabase.getInstance();
-        final FirebaseAuth mAuth=FirebaseAuth.getInstance();
+        aileList = new ArrayList<AileModel>();
+        listView = (ListView) view.findViewById(R.id.listView);
+        database = FirebaseDatabase.getInstance();
+        final FirebaseAuth mAuth = FirebaseAuth.getInstance();
         firebaseUser = mAuth.getCurrentUser();
-        //databaseReferenceAile = FirebaseDatabase.getInstance().getReference("aile");
-//        final DatabaseReference dbRef=database.getReference("aile");
+
+        btnGeri=(Button)view.findViewById(R.id.btnGeri);
+        btnGeri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),AileNavDrawer.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
 
         final DatabaseReference dbRef = database.getReference("aile").child(
                 firebaseUser.getUid());
@@ -108,14 +97,10 @@ public class KisiselBilgiAileFragment extends Fragment {
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 aileList.clear();
-//                for (DataSnapshot ds:dataSnapshot.getChildren()){
-                    AileModel aileModel = dataSnapshot.getValue(AileModel.class);
-//                    String aileAd = ds.child("aileAd").getValue().toString();
-//                    String aileSoyad = ds.child("aileSoyad").getValue().toString();
-//                    String aileMail = ds.child("aileMail").getValue().toString();
-                    aileList.add(new AileModel(aileModel.getAileAd(),aileModel.getAileSoyad(),aileModel.getAileMail()));
-//                }
-                CustomAdapterAile adapter=new CustomAdapterAile(getActivity(),aileList);
+                AileModel aileModel = dataSnapshot.getValue(AileModel.class);
+                aileList.add(new AileModel(aileModel.getAileAd(), aileModel.getAileSoyad(), aileModel.getAileMail()));
+
+                CustomAdapterAile adapter = new CustomAdapterAile(getActivity(), aileList);
                 listView.setAdapter(adapter);
                 dbRef.removeEventListener(this);
 
@@ -130,23 +115,12 @@ public class KisiselBilgiAileFragment extends Fragment {
         return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
-    }
 
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
+    }
 
     @Override
     public void onDetach() {
@@ -154,18 +128,9 @@ public class KisiselBilgiAileFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
 }
