a86731a9b36936613a7b94c1a62a196f773bd6a5
YardimciHazir <30574709+YardimciHazir@users.noreply.github.com>
2017-09-09 00:20:20 +0300

    Mesajlaşma arama butonları eklendi.

--- app/src/main/java/com/merveyanar/ilkprojeyardimcim/MesajlarAileFragment.java
+++ app/src/main/java/com/merveyanar/ilkprojeyardimcim/MesajlarAileFragment.java
@@ -1,25 +1,18 @@
 package com.merveyanar.ilkprojeyardimcim;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MesajlarAileFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MesajlarAileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MesajlarAileFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    Button telefon,mesaj,email,websitesi;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -30,18 +23,9 @@ public class MesajlarAileFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
 
     public MesajlarAileFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MesajlarAileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MesajlarAileFragment newInstance(String param1, String param2) {
         MesajlarAileFragment fragment = new MesajlarAileFragment();
         Bundle args = new Bundle();
@@ -63,8 +47,72 @@ public class MesajlarAileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mesajlar_aile, container, false);
+       View view=inflater.inflate(R.layout.fragment_mesajlar_aile, container, false);
+        telefon = (Button)view.findViewById(R.id.button1);//Butonları uygulamaya tanıtıyoruz
+        mesaj = (Button)view.findViewById(R.id.button2);//Butonları uygulamaya tanıtıyoruz
+        email = (Button)view.findViewById(R.id.button3);//Butonları uygulamaya tanıtıyoruz
+        websitesi = (Button)view.findViewById(R.id.button4);//Butonları uygulamaya tanıtıyoruz
+        telefon.setOnClickListener(new View.OnClickListener() {//Telefon aç butonu tıklanınca
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:1234567"));
+                startActivity(intent);
+            }
+        });
+
+        mesaj.setOnClickListener(new View.OnClickListener() {//mesaj at butonu tıklanınca
+
+            @Override
+            public void onClick(View v) {
+
+                String telNo = "5321111111";//mesaj gönderilecek numara
+                String mesaj = "Bu mesaj mobilhanem.com tarafından gönderilmiştir."; //mesaj
+
+                //Bu kısım mesaj application açmak için.Yani sms uygulaması açılır. Numara kısmı ve mesaj kısmı otomatik doldurulur.
+
+                Intent mesajGonder = new Intent(Intent.ACTION_VIEW);
+                mesajGonder.setData(Uri.parse("sms:"+ telNo));
+                mesajGonder.putExtra("sms_body", mesaj);
+                startActivity(mesajGonder);
+
+
+			/*		Bu kısım direk mesaj atmak için	.Herhangi bir uygulama açmaz direk mesaj atar.Bunun yerine üsteki daha sağlıklı
+				SmsManager smsManager = SmsManager.getDefault();
+				smsManager.sendTextMessage(telNo, null, mesaj, null, null);
+			*/
+            }
+        });
+
+        email.setOnClickListener(new View.OnClickListener() {//Email at butonu tıklanınca
+
+            @Override
+            public void onClick(View v) {
+
+                Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Konu");//Email konusu
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "Bu Mail mobilhanem.com tarafından gönderilmiştir.");//Email içeriği
+                startActivity(Intent.createChooser(emailIntent, "E-mail Göndermek için Seçiniz:")); //birden fazla email uygulaması varsa seçmek için
+                String aEmailList[] = { "bilgi@mobilhanem.com" };  //Mail gönderielecek kişi.Birden fazla ise virgülle ayırarak yazılır
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, aEmailList);
+                startActivity(emailIntent);
+            }
+        });
+
+        websitesi.setOnClickListener(new View.OnClickListener() { //Web Sitesi aç butonu
+
+            @Override
+            public void onClick(View v) {//Web sitesini tarayıcıda açar
+
+                String url = "http://www.mobilhanem.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        return  view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -74,33 +122,12 @@ public class MesajlarAileFragment extends Fragment {
         }
     }
 
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
